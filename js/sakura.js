5808397520a8f23bf64751daf46dcc41fbc0483a
Pwcx <3384572853@qq.com>
2020-10-25 09:27:19 +0800

    Site updated: 2020-10-25 09:27:18

---
+++ js/sakura.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+	</body>
+</html>
+<script src="https://cdn.jsdelivr.net/gh/yremp/yremp-js@1.5/sakura.js"></script>
